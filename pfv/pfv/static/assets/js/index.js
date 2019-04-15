7dff0fadd9a747167415b15086aa6d82ad302df0
Cristi <cristicismas99@gmail.com>
2019-04-15 14:44:57 +0300

    Make flickity render only on videos page

--- pfv/pfv/static/assets/js/index.js
+++ pfv/pfv/static/assets/js/index.js
@@ -1,28 +1,29 @@
-const carousel = document.querySelector('.video-carousel')
+const carousel = document.querySelector(".video-carousel");
 
-var flkty = new Flickity(carousel, {
-  cellAlign: 'center',
-  wrapAround: true,
-  pageDots: false
-});
+if (carousel) {
+  var flkty = new Flickity(carousel, {
+    cellAlign: "center",
+    wrapAround: true,
+    pageDots: false
+  });
 
-if (window.innerWidth < 700) {
-  flkty.destroy();
-}
-
-var lastWidth = window.innerWidth;
-
-window.onresize = function() {
   if (window.innerWidth < 700) {
     flkty.destroy();
-  } else if (lastWidth < 700 && window.innerWidth > 700) {
-    flkty = new Flickity(carousel, {
-      cellAlign: 'center',
-      wrapAround: true,
-      pageDots: false
-    });
   }
 
-  lastWidth = window.innerWidth;
-}
+  var lastWidth = window.innerWidth;
 
+  window.onresize = function() {
+    if (window.innerWidth < 700) {
+      flkty.destroy();
+    } else if (lastWidth < 700 && window.innerWidth > 700) {
+      flkty = new Flickity(carousel, {
+        cellAlign: "center",
+        wrapAround: true,
+        pageDots: false
+      });
+    }
+
+    lastWidth = window.innerWidth;
+  };
+}
