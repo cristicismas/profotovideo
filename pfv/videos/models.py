df6a701846c957c5c36513f63d671dca5556ee54
Cristi Cismas <cristicismas99@gmail.com>
2019-04-24 19:57:21 +0300

    Make video and photo date field optional

--- pfv/videos/models.py
+++ pfv/videos/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Video(models.Model):
     url = models.URLField(max_length=200)
     title = models.CharField(max_length=70, blank=True)
-    date = models.DateField(auto_now=False, auto_now_add=False)
+    date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
     isFeatured = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.url
     
