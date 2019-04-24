382d5b5950f11918ef18e84e572c21630b19e7c4
Cristi Cismas <cristicismas99@gmail.com>
2019-04-24 19:35:24 +0300

    Add an isFeatured field to photos and videos

--- pfv/photos/models.py
+++ pfv/photos/models.py
@@ -5,6 +5,7 @@ class Photo(models.Model):
     thumbnail = models.URLField(max_length=200, blank=True)
     title = models.CharField(max_length=70, blank=True)
     date = models.DateField(auto_now=False, auto_now_add=False)
+    isFeatured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.url
