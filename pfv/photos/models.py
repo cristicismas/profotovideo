7d02ba2ad4b068f52d86567d5dadd99f41b57389
Cristi Cismas <cristicismas99@gmail.com>
2019-04-29 16:11:46 +0300

    Make dates nullable

--- pfv/photos/models.py
+++ pfv/photos/models.py
@@ -4,7 +4,7 @@ class Photo(models.Model):
     url = models.URLField(max_length=200)
     thumbnail = models.URLField(max_length=200, blank=True)
     title = models.CharField(max_length=70, blank=True)
-    date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
+    date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
     isFeatured = models.BooleanField(default=False)
 
     def __str__(self):
--- pfv/videos/models.py
+++ pfv/videos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Video(models.Model):
     url = models.URLField(max_length=200)
     title = models.CharField(max_length=70, blank=True)
-    date = models.DateField(auto_now=False, auto_now_add=False, blank=True)
+    date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
     isFeatured = models.BooleanField(default=False)
 
     def __str__(self):
