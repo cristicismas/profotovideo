f8001f4a996f8e672b1a475e62a4ddc0c838919d
Cristi Cismas <cristicismas99@gmail.com>
2019-05-03 17:11:44 +0300

    Customize admin css some more and make some changes to the admin model for videos and photos

--- photos/admin.py
+++ photos/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Photo
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'isFeatured', 'url')
+  list_display_links = ('id', 'title', 'url')
+  list_editable = ('isFeatured',)
+  list_filter = ('isFeatured',)
+
+admin.site.register(Photo, PhotoAdmin)
--- videos/admin.py
+++ videos/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Video
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'isFeatured', 'url')
+  list_display_links = ('id', 'title', 'url')
+  list_editable = ('isFeatured',)
+  list_filter = ('isFeatured',)
+
+admin.site.register(Video, VideoAdmin)
