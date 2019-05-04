c37d9897757e846ee387141b45d3af01abb2cac8
Cristi Cismas <cristicismas99@gmail.com>
2019-05-04 15:45:24 +0300

    Make video url a display_link

--- content/admin.py
+++ content/admin.py
@@ -18,7 +18,7 @@ admin.site.register(Photo, PhotoAdmin)
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('id', 'isFeatured', 'url')
-    list_display_links = ('id',)
+    list_display_links = ('id', 'url')
     list_editable = ('isFeatured',)
     list_filter = ('isFeatured',)
 
