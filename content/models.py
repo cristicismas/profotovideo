baddebf4d6bb8e6168c7c3a3e898baccae97e136
Cristi Cismas <cristicismas99@gmail.com>
2019-05-04 15:47:07 +0300

    Override video's save method, so if the isFeatured is true, all other videos will change isFeatured to false

--- content/models.py
+++ content/models.py
@@ -23,6 +23,11 @@ class Photo(models.Model):
 class Video(models.Model):
     url = models.URLField(max_length=200)
     isFeatured = models.BooleanField(default=False)
+    
+    def save(self, *args, **kwargs):
+        if self.isFeatured == True:
+            Video.objects.filter(isFeatured=True).update(isFeatured=False)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.url
