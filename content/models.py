ea9f5e8a0d64214ac098f4d55f636b87b3c9fb86
Cristi Cismas <cristicismas99@gmail.com>
2019-05-04 14:27:27 +0300

    Remove useless fields in photos and videos models

--- content/models.py
+++ content/models.py
@@ -14,10 +14,6 @@ class Album(models.Model):
 
 class Photo(models.Model):
     url = models.URLField(max_length=200)
-    thumbnail = models.URLField(max_length=200, blank=True)
-    title = models.CharField(max_length=70, blank=True)
-    date = models.DateField(
-        auto_now=False, auto_now_add=False, blank=True, null=True)
     isFeatured = models.BooleanField(default=False)
 
     def __str__(self):
@@ -26,9 +22,6 @@ class Photo(models.Model):
 
 class Video(models.Model):
     url = models.URLField(max_length=200)
-    title = models.CharField(max_length=70, blank=True)
-    date = models.DateField(
-        auto_now=False, auto_now_add=False, blank=True, null=True)
     isFeatured = models.BooleanField(default=False)
 
     def __str__(self):
