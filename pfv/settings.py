b9cfa40169f0d015e262ba7525570d884b298575
Cristi Cismas <cristicismas99@gmail.com>
2019-05-09 00:14:16 +0300

    Add proxy https rule for prod

--- pfv/settings.py
+++ pfv/settings.py
@@ -16,11 +16,16 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 ENV = os.getenv('ENV')
 
 DEBUG = False if ENV == 'PRODUCTION' else True
+
 SECURE_SSL_REDIRECT = True if ENV == 'PRODUCTION' else False
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') if ENV == 'PRODUCTION' else None
+
 SECURE_CONTENT_TYPE_NOSNIFF = True if ENV == 'PRODUCTION' else False
 SECURE_BROWSER_XSS_FILTER = True if ENV == 'PRODUCTION' else False
+
 CSRF_COOKIE_SECURE = True if ENV == 'PRODUCTION' else False
 SESSION_COOKIE_SECURE = True if ENV == 'PRODUCTION' else False
+
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
