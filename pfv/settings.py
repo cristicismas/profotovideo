1c693806a587feaa3306fdeee2a83d6e1a51efe7
Cristi Cismas <cristicismas99@gmail.com>
2019-05-08 22:07:27 +0300

    Make secret key an environment variable

--- pfv/settings.py
+++ pfv/settings.py
@@ -27,7 +27,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6c%ob5o@moi^7(meng1st^h7pdal__h8dl&q+#!*^j7#kwocpy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
