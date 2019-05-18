27f9a305cd1ceb425c2d860f04a04728e27e4518
Cristi Cismas <cristicismas99@gmail.com>
2019-05-18 20:08:56 +0300

    Order photos and videos by updated DateTimeField

--- content/models.py
+++ content/models.py
@@ -16,6 +16,7 @@ class Album(models.Model):
 class Photo(models.Model):
     url = models.URLField(max_length=200, unique=True)
     isFeatured = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now=True)
 
     def preview(self):
         return mark_safe('<img src="%s" class="image-preview" />' % self.url)
@@ -23,10 +24,14 @@ class Photo(models.Model):
     def __str__(self):
         return self.url
 
+    class Meta:
+        ordering=['-updated']
+
 
 class Video(models.Model):
     url = models.URLField(max_length=200, unique=True)
     isFeatured = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now=True)
     
     def save(self, *args, **kwargs):
         if self.isFeatured == True:
@@ -35,3 +40,6 @@ class Video(models.Model):
 
     def __str__(self):
         return self.url
+
+    class Meta:
+        ordering=['-updated']
