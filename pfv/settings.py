f642bbb61ec8de3ebb83e1725d3799d433a4cdab
Cristi Cismas <cristicismas99@gmail.com>
2019-11-12 16:53:29 +0200

    Add live reload

--- pfv/settings.py
+++ pfv/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'content.apps.ContentConfig'
+    'content.apps.ContentConfig',
+    'livereload'
 ]
 
 MIDDLEWARE = [
