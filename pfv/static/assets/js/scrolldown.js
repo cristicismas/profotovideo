0d35a76318fca97593661c93c5b2af3d115345f0
Cristi Cismas <cristicismas99@gmail.com>
2019-12-07 12:25:05 +0200

    When scrolldown arrow is clicked, scroll to the photos' title instead of the #photos section

--- pfv/static/assets/js/scrolldown.js
+++ pfv/static/assets/js/scrolldown.js
@@ -3,7 +3,9 @@ const scrollDownButton = document.getElementById('scrolldown-btn');
 scrollDownButton.addEventListener('click', function (e) {
     e.preventDefault();
 
-    document.getElementById('photos').scrollIntoView({
+    const photosTitle = document.getElementsByClassName('title')[0];
+
+    photosTitle.scrollIntoView({
         behavior: 'smooth',
         block: 'start'
     });
